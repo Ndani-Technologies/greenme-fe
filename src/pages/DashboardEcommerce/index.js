ee2f572dbd8828ed25e3dc65046182cfce7d6a4c
Martin Macharia <mosmartin5@gmail.com>
2023-03-24 13:03:28 +0100

    :construction: Initial Commit

---
+++ src/pages/DashboardEcommerce/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Col, Container, Row } from "reactstrap";
+
+const DashboardEcommerce = () => {
+  document.title = "Dashboard | Velzon - React Admin & Dashboard Template";
+
+  const [rightColumn, setRightColumn] = useState(true);
+  const toggleRightColumn = () => {
+    setRightColumn(!rightColumn);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+          <Row>
+            
+            
+          </Row>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default DashboardEcommerce;
